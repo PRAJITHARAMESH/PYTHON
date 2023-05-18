1ce9d2dd694ba9f5d13f5033134029c29a2c48cb
PRAJITHARAMESH <130755967+PRAJITHARAMESH@users.noreply.github.com>
2023-05-18 11:47:01 +0530

    Add files via upload

---
+++ selective program/selective5.py
@@ -0,0 +1,12 @@
+def is_pythagorean_triple(n1, n2, n3):
+    if (n1**2 + n2**2 == n3**2):
+        return 1
+    else:
+        return 0
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+n3 = int(input("Enter the third number: "))
+
+result = is_pythagorean_triple(n1, n2, n3)
+print(result)
